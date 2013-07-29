ef95d310912ed555b6a953f5c2cacb1fb01dc23c
Tobias Knell <knell@synyx.de>
2013-07-29 16:45:05 +0200

    added ObjectInformation object
    * accumulates variuous information needed to create the objects

---
+++ src/main/java/org/synyx/beanfiller/ObjectInformation.java
@@ -0,0 +1,119 @@
+
+package org.synyx.beanfiller;
+
+import org.synyx.beanfiller.strategies.AbstractCreatorStrategy;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+
+
+/**
+ * Contains Meta-Information about an Object that is needed in the creation proccess.
+ *
+ * @author  Tobias Knell - knell@synyx.de
+ */
+public class ObjectInformation {
+
+    private Class clazz;
+    private Field field;
+    private Method accessor;
+    private Type type;
+    private AbstractCreatorStrategy strategy;
+    private String path;
+
+    /**
+     * @param  clazz  class of the object
+     * @param  field  the field, the object should be put into
+     * @param  type  type of the object to create.
+     * @param  accessor  accessor to set the field.
+     * @param  path  the object path of the object
+     */
+    public ObjectInformation(Class clazz, Field field, Type type, Method accessor, String path) {
+
+        this.clazz = clazz;
+        this.field = field;
+        this.type = type;
+        this.accessor = accessor;
+        this.path = path;
+    }
+
+    public Class getClazz() {
+
+        return clazz;
+    }
+
+
+    public void setClazz(Class clazz) {
+
+        this.clazz = clazz;
+    }
+
+
+    public Field getField() {
+
+        return field;
+    }
+
+
+    public void setField(Field field) {
+
+        this.field = field;
+    }
+
+
+    public Type getType() {
+
+        return type;
+    }
+
+
+    public void setType(Type type) {
+
+        this.type = type;
+    }
+
+
+    public Method getAccessor() {
+
+        return accessor;
+    }
+
+
+    public void setAccessor(Method accessor) {
+
+        this.accessor = accessor;
+    }
+
+
+    public AbstractCreatorStrategy getStrategy() {
+
+        return strategy;
+    }
+
+
+    public void setStrategy(AbstractCreatorStrategy strategy) {
+
+        this.strategy = strategy;
+    }
+
+
+    public String getPath() {
+
+        return path;
+    }
+
+
+    public void setPath(String path) {
+
+        this.path = path;
+    }
+
+
+    @Override
+    public String toString() {
+
+        return "ObjectInformation{" + "clazz=" + clazz + ", field=" + field + ", accessor=" + accessor + ", type="
+            + type + ", strategy=" + strategy + ", path=" + path + '}';
+    }
+}
