402a9af308ac9fccacbadd79d6ec84b17fe80e63
Tobias Knell <knell@synyx.de>
2024-02-21 12:54:02 +0100

    Fix constructor creation with generic types and collections

---
+++ src/test/java/org/synyx/beanfiller/testobjects/ArrayConstructorObject.java
@@ -0,0 +1,15 @@
+package org.synyx.beanfiller.testobjects;
+
+public class ArrayConstructorObject {
+
+    private final String[] values;
+
+    public ArrayConstructorObject(String[] values){
+
+        this.values = values;
+    }
+
+    public String[] getValues() {
+        return values;
+    }
+}
